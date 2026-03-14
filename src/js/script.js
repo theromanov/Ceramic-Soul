16d9a89817f7465ecd76fa90a17b11b08ee3b2e9
Roman <romansyplyviy@gmail.com>
2026-03-14 19:56:12 +0100

    feat: add tabs functionality to script.js

--- src/js/script.js
+++ src/js/script.js
@@ -45,3 +45,20 @@ try {
     },
   });
 } catch (e) {}
+
+try {
+  const tabs = document.querySelectorAll(".catalog__tab");
+  const contents = document.querySelectorAll(".catalog__content-item");
+
+  tabs.forEach((tab, index) => {
+    tab.addEventListener("click", () => {
+      tabs.forEach((t) => t.classList.remove("catalog__tab_active"));
+      contents.forEach((c) => (c.style.display = "none"));
+
+      tab.classList.add("catalog__tab_active");
+      contents[index].style.display = "flex";
+    });
+  });
+
+  contents.forEach((c, i) => (c.style.display = i === 0 ? "flex" : "none"));
+} catch (e) {}
