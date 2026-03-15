a6f050befe79940bfbd0cb45fba70e36ff3d8a7f
Roman <romansyplyviy@gmail.com>
2026-03-15 14:37:56 +0100

    chore: configure vite dependencies and  vite-plugin-image-min for image optimization

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,45 @@
-import { dirname, resolve } from "node:path";
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
+import viteImagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
   build: {
-    rolldownOptions: {
+    rollupOptions: {
       input: {
-        main: resolve(import.meta.dirname, "index.html"),
-        catalog: resolve(import.meta.dirname, "catalog.html"),
-        blog: resolve(import.meta.dirname, "blog.html"),
-        about: resolve(import.meta.dirname, "about.html"),
+        main: resolve(process.cwd(), "index.html"),
+        catalog: resolve(process.cwd(), "catalog.html"),
+        blog: resolve(process.cwd(), "blog.html"),
+        about: resolve(process.cwd(), "about.html"),
       },
     },
   },
+  plugins: [
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 90,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: "removeViewBox",
+          },
+          {
+            name: "removeEmptyAttrs",
+            active: false,
+          },
+        ],
+      },
+    }),
+  ],
 });
